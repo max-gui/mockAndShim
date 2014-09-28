62741a7ab7eb105e15c634e7c1e0f555b2eac09b
jimmy <playstation.gui@gmail.com>
2014-09-28 21:47:22 +0800

    init

---
+++ mockAndShim/staticTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mockAndShim
+{
+    static public class staticTest
+    {
+        static public string test(int para)
+        {
+            return para.ToString();
+        }
+    }
+}
---
+++ processConsoleTests/ProgramTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using processConsole;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.QualityTools.Testing.Fakes;
+namespace processConsole.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void testMockTest()
+        {
+            using(ShimsContext.Create())
+            {
+                mockAndShim.Fakes.ShimstaticTest.testInt32 = (b) =>
+                    {
+                        var res = b.ToString() + "haha";
+                        return res;
+                    };
+
+                var para = 100;
+                var testMS = test.testMock(para);
+                Console.WriteLine(testMS);
+
+                Assert.AreEqual(testMS, para.ToString() + "haha");
+            }
+        }
+    }
+}
